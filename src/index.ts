4b82b7acfc56387bc937392b1d5111357da0bef3
MocanaAtBroad <mocana@broadinstitute.org>
2020-12-02 14:18:25 -0500

    1 file added

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter 2</h1>`;
